9ac37ad21a436fc11f4ec35c0c45517b42339d62
shahnawazm786 <49604292+shahnawazm786@users.noreply.github.com>
2021-08-24 13:03:22 +0530

    commit

--- src/test/java/CollectionExample/ListArrayExample.java
+++ src/test/java/CollectionExample/ListArrayExample.java
@@ -1,2 +1,16 @@
-package CollectionExample;public class ListArrayExample {
+package CollectionExample;
+import java.util.*;
+public class ListArrayExample {
+    public static void main(String[] args){
+        listArrayMethod();
+    }
+    public static void listArrayMethod(){
+        ArrayList<String> arrayList=new ArrayList<String>();
+        arrayList.add("Java");
+        arrayList.add("Oracle");
+        arrayList.add("DBMS");
+        for(String s: arrayList){
+            System.out.println(s);
+        }
+    }
 }
